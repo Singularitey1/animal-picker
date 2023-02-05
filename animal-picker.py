19e749934fefff9e17a9e113a577b393b93cee73
Kevin <50928782+Singularitey1@users.noreply.github.com>
2023-02-04 22:34:35 -0500

    Update animal-picker.py

--- animal-picker.py
+++ animal-picker.py
@@ -3,24 +3,32 @@ from icrawler.builtin import GoogleImageCrawler
 import os
 import shutil
 import wikipedia
+import random
 
 # emotion detection
-face_analysis = DeepFace.analyze(img_path = "toaster.jpg", enforce_detection=False)
+face_analysis = DeepFace.analyze(img_path = "guy3.webp", enforce_detection=False)
 dominantEmotion = face_analysis[0]["dominant_emotion"]
 face_analysis[0]["emotion"].pop(dominantEmotion)
 secondEmotion = max(face_analysis[0]["emotion"])
 
 # sets favorite animal and search query
 print(dominantEmotion + " and " + secondEmotion)
-favorite_animal = "komodo dragon"
+favorite_animal = "can"
 query = favorite_animal + " " + dominantEmotion + " and " + secondEmotion + " animal"
 
-# finds image using google image crawler
+# finds images using google image crawler
 if os.path.exists("images"):
     shutil.rmtree("images")
 image_crawler = GoogleImageCrawler(storage = {"root_dir": r'images'})
-filter = dict(type="photo")
-image_crawler.crawl(keyword = query, filters=filter, max_num = 1)
+image_crawler.crawl(keyword = query, max_num = 3)
+
+# keeps one random image
+randInt = random.randint(1,3)
+for file in os.listdir("images"):
+    if file != "00000" + str(randInt) + ".jpg":
+        os.remove("images/" + file)
+    else:
+        os.rename("images/" + file, "images/animalImage.jpg")
 
 # function to search wikipedia for the inputted favorite animal
 def searchWikipedia():
@@ -28,14 +36,20 @@ def searchWikipedia():
     if not search:
         search = wikipedia.suggest(favorite_animal)
         if search == None:
-            return "You may want to rewrite your favorite animal."
-        result = wikipedia.summary(search + search[-1])
+            return ["You may want to rewrite your favorite animal.", "No link found."]
+        try:
+            result = wikipedia.summary(search, auto_suggest=False)
+            link = wikipedia.page(search, auto_suggest=False).url
+        except:
+            return ["Wikipedia not found. Try to be specific.", "No link found."]
     else:
         try:
-            result = wikipedia.summary(search[0] + search[0][-1])
+            result = wikipedia.summary(search[0], auto_suggest=False)
+            link = wikipedia.page(search[0], auto_suggest=False).url
         except:
-            return "Wikipedia not found."
-    return result
+            return ["Wikipedia not found. Try to be specific.", "No link found."]
+    return [result, link]
 
 # prints the wikipedia summary of the favorite animal
-print(searchWikipedia())
+print(searchWikipedia()[0])
+print(searchWikipedia()[1])
